aa3fe5082f79aeceba38d94d4d035df0147ca0c4
Neil Watkins <neil.watkins1@sky.com>
2019-04-09 11:01:41 +0100

    comments app prior to refactoring

---
+++ src/components/Comment.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Comment extends Component{
+
+  render(){
+    const { children, author } = this.props
+
+    return(
+
+    <div className="comment">
+    <h4>{children}</h4>
+    <p>{author}</p>
+    </div>
+    );
+  }
+}
+export default Comment;
---
+++ src/containers/CommentBox.js
@@ -0,0 +1,37 @@
+import React, { Component} from 'react';
+import CommentList from '../components/CommentList';
+import CommentForm from '../components/CommentForm';
+
+class CommentBox extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    data: [
+      {id: 1, author: 'Louise', text: 'I love cheese'},
+      {id: 2, author: 'Colin', text: 'Is it hometime?!'}
+    ]
+  }
+  this.handleCommentSubmit = this.handleCommentSubmit.bind(this)
+}
+
+handleCommentSubmit(comment){
+  comment.id = Date.now()
+  const updatedComments = [...this.state.data, comment]
+  this.setState({data: updatedComments })
+
+
+
+}
+  render(){
+    return(
+      <div className="comment-box">
+      <h2>Add a Comment</h2>
+      <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+      <h2>Comments List</h2>
+      <CommentList data={this.state.data}/>
+      </div>
+    )
+  }
+}
+
+export default CommentBox;
